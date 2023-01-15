6eec6525a695dbf21747b94ce6a028e14ede3564
MYEONGGI <smgsmg226@gmail.com>
2023-01-15 16:04:06 +0900

    Update

--- 밑바닥부터 시작하는 딥러닝 1/ch03/relu.py
+++ 밑바닥부터 시작하는 딥러닝 1/ch03/relu.py
@@ -6,8 +6,8 @@ import matplotlib.pylab as plt
 def relu(x):
     return np.maximum(0, x)
 
-x = np.arange(-5.0, 5.0, 0.1)
+x = np.arange(-5.0, 8.0, 0.1)
 y = relu(x)
 plt.plot(x, y)
-plt.ylim(-1.0, 5.5)
+plt.ylim(-1.0, 20.5)
 plt.show()
